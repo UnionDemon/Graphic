00e1657c18a47d4a4ca6f8c8797bd4e44c83e8b5
Егор Антонянц <bax2014@mail.ru>
2021-10-07 00:18:06 +0700

    fix

--- SFML_Tutorial/main.cpp
+++ SFML_Tutorial/main.cpp
@@ -1,64 +1,134 @@
 #include <SFML/Graphics.hpp>
+#include <string>
+#include <iostream>
 
 using namespace sf;
 
 int main()
 {
-	RenderWindow app(VideoMode(400, 400), "SFML Works!");
-	// Ширина клетки
-	int width = 32;
-	int gridLogic[9][9];
-	int gridView[9][9];
-	int fieldCell = 8;
+	RenderWindow app(VideoMode(1024, 768), "Minesweeper!", Style::Close | sf::Style::Titlebar);
 
-	// Загрузка текстуры и создание спрайта ДЛЯ СИНИХ КЛЕТОК
-	Texture t;
-	t.loadFromFile("D:\\images\\texturesB.jpg");
-	Sprite s(t);
+	// Ширина поля
+	int w = 66;
+	int gridLogic[8][8];
+	int gridView[8][8];
 
-	// Загрузка текстуры и создание спрайта ДЛЯ БЕЛО-ЖЕЛТЫХ КЛЕТОК
-	Texture text;
-	text.loadFromFile("D:\\images\\texturesYW.jpg");
-	Sprite g(text);
+	// Загрузка текстуры и создание спрайта
+	Texture white;
+	Texture blue;
+	white.loadFromFile("D:\\images\\White.png");
+	blue.loadFromFile("D:\\images\\Blue.png");
 
-	for (int i = 1; i <= fieldCell; i++)
-		for (int j = 1; j <= fieldCell; j++)
+	Sprite whiteSprite(white);
+	Sprite blueSprite(blue);
+
+	Font font;
+	font.loadFromFile("D:\\SFML_Tutorial\\Debug\\CyrilicOld.TTF");
+
+	Text txt;
+	txt.setFont(font);
+	txt.setPosition(50, 50);
+	txt.setString("TESSSSSTT");
+	txt.setCharacterSize(24);
+	txt.setFillColor(Color::Black);
+	txt.setStyle(Text::Bold);
+
+	//Заполнить все квадраты синим
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++)
 		{
-			gridView[i][j] = fieldCell;
+			if (i % 2 == 0)
+			{
+				if (j % 2 == 0) {
+					gridView[i][j] = 1;
+
+				}
+				else {
+					gridView[i][j] = 0;
+				}
+			}
+			else {
+				if (j % 2 == 0) {
+					gridView[i][j] = 0;
+
+				}
+				else {
+					gridView[i][j] = 1;
+				}
+			}
 		}
 
 	while (app.isOpen())
 	{
-		app.clear(Color(255, 255, 255, 0));
-		Event event;
-		while (app.pollEvent(event))
+		Event e;
+
+		while (app.pollEvent(e))
 		{
-			if (event.type == Event::Closed)
+			if (e.type == Event::Closed)
 				app.close();
 		}
-		for (int i = 1; i <= fieldCell; i+=2)
-			for (int j = 1; j <= fieldCell; j+=1)
+
+		app.clear(Color::Black);
+
+		//ОТРИСОВКА ПЛИТ ДОСКИ
+		RectangleShape rectangle1(Vector2f(594.f, 594.f));
+		//Перемещаем его в нижний ряд справа от многоугольника
+		rectangle1.move(33, 33);
+		//Устанавливаем ему цвет
+		rectangle1.setFillColor(Color(255, 255, 255));
+		//Отрисовка прямоугольника
+		app.draw(rectangle1);
+
+		RectangleShape rectangle2(Vector2f(530.f, 530.f));
+		//Перемещаем его в нижний ряд справа от многоугольника
+		rectangle2.move(64, 64);
+		//Устанавливаем ему цвет
+		rectangle2.setFillColor(Color(0, 0, 0));
+		//Отрисовка прямоугольника
+		app.draw(rectangle2);
+
+		for (int i = 0; i < 8; i++)
+			for (int j = 0; j < 8; j++)
 			{
-				// Вырезаем из спрайта нужный нам квадратик текстуры
-				s.setTextureRect(IntRect(gridView[i][j] * width, 0, width, width));
-				// Устанавливаем его в заданную позицию...
-				s.setPosition(i * width, j * width);
-				// ... и отрисовываем
-				app.draw(s);
+				if (gridView[i][j] == 1)
+				{
+					// Устанавливаем его в заданную позицию...
+					whiteSprite.setPosition((i + 1) * w, (j + 1) * w);
+					// ... и отрисовываем
+					app.draw(whiteSprite);
+				}
+				else {
+					// Устанавливаем его в заданную позицию...
+					blueSprite.setPosition((i + 1) * w, (j + 1) * w);
+					// ... и отрисовываем
+					app.draw(blueSprite);
+				}
 			}
-		for (int i = 2; i <= fieldCell; i+=2)
-			for (int j = 1; j <= fieldCell; j+=1)
-			{
-				// Вырезаем из спрайта нужный нам квадратик текстуры
-				g.setTextureRect(IntRect(gridView[i][j] * width, 0, width, width));
-				// Устанавливаем его в заданную позицию...
-				g.setPosition(i * width, j * width);
-				// ... и отрисовываем
-				app.draw(g);
-			}
+
+		//Отрисовка букв и цифр
+		int digitStartPosX = 40;
+		int digitPosY = 83;
+		for (int i = 1; i < 9; i += 1, digitPosY += 66)
+		{
+			txt.setPosition(digitStartPosX, digitPosY);
+			txt.setString(std::to_string(i));
+			app.draw(txt);
+		}
+
+		int charPosX = 83;
+		int charStartPosY = 33;
+		for (char c = 'a'; c < 'i'; c += 1, charPosX += 66)
+		{
+			txt.setPosition(charPosX, charStartPosY);
+			txt.setString(c);
+			app.draw(txt);
+		}
+		
+		
+
 		// Отображаем всю композицию на экране
 		app.display();
-	}
 
+	}
 	return 0;
 }
